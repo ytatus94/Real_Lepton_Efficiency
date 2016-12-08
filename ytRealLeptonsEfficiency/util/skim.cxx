7234f557b0d6d47659d18c385342348bc7395239
ytshen <ytshen@ytshenmac.dyndns.cern.ch>
2016-12-08 16:49:09 +0100

    Change space

--- ytRealLeptonsEfficiency/util/skim.cxx
+++ ytRealLeptonsEfficiency/util/skim.cxx
@@ -106,171 +106,171 @@ int main( int argc, char* argv[] ) {
     double filter_efficiency = 1.;
     double derivation_stat_weights = 1.;
 
-	if (isMC) {
-		cout << "Read MC files..." << endl;
-		inputFilePath = "/raid05/users/shen/Ximo_ntuples/v44/MC"; // no slash (/) at the end.
-		// For cutflow study
-		if (process == "4topSM") {
-			//inputFilePath = "/UserDisk2/yushen/Ximo_ntuples/v44/MC/user.jpoveda.t0789_v44.410080.MadGraphPythia8EvtGen_A14NNPDF23_4topSM.DAOD_SUSY2.s2608_r7725_p2666_output.root";
-			//SH::ScanDir().filePattern("user.jpoveda.9048853._000001.output.root").scan(sh, inputFilePath);
-			SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*4topSM*").scan(sh, inputFilePath); // Get all root files in this dataset
+    if (isMC) {
+        cout << "Read MC files..." << endl;
+        inputFilePath = "/raid05/users/shen/Ximo_ntuples/v44/MC"; // no slash (/) at the end.
+        // For cutflow study
+        if (process == "4topSM") {
+            //inputFilePath = "/UserDisk2/yushen/Ximo_ntuples/v44/MC/user.jpoveda.t0789_v44.410080.MadGraphPythia8EvtGen_A14NNPDF23_4topSM.DAOD_SUSY2.s2608_r7725_p2666_output.root";
+            //SH::ScanDir().filePattern("user.jpoveda.9048853._000001.output.root").scan(sh, inputFilePath);
+            SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*4topSM*").scan(sh, inputFilePath); // Get all root files in this dataset
 
-			cross_section = 0.0091622 * 1000.; // in fb
-			k_factor = 1.0042;
-			filter_efficiency = 1.;
-			derivation_stat_weights = 1.988000e+05;	
-		}
-		// For real lepton efficiency study
-		else if (process == "Zee") {
-			//SH::ScanDir().filePattern("Zee_merged.root").scan(sh, inputFilePath); // Get specific root file
-			SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*Zee*").scan(sh, inputFilePath); // Get all root files in this dataset
+            cross_section = 0.0091622 * 1000.; // in fb
+            k_factor = 1.0042;
+            filter_efficiency = 1.;
+            derivation_stat_weights = 1.988000e+05; 
+        }
+        // For real lepton efficiency study
+        else if (process == "Zee") {
+            //SH::ScanDir().filePattern("Zee_merged.root").scan(sh, inputFilePath); // Get specific root file
+            SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*Zee*").scan(sh, inputFilePath); // Get all root files in this dataset
 
-			cross_section = 1901.2 * 1000.; // in fb
-			k_factor = 1.026;
-			filter_efficiency = 1.;
-			derivation_stat_weights = 37868855526.2;
-		}
-		else if (process == "Zmumu") {
-			//SH::ScanDir().filePattern("Zmumu_merged.root").scan(sh, inputFilePath); // Get specific root file
-			SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*Zmumu*").scan(sh, inputFilePath); // Get all root files in this dataset
+            cross_section = 1901.2 * 1000.; // in fb
+            k_factor = 1.026;
+            filter_efficiency = 1.;
+            derivation_stat_weights = 37868855526.2;
+        }
+        else if (process == "Zmumu") {
+            //SH::ScanDir().filePattern("Zmumu_merged.root").scan(sh, inputFilePath); // Get specific root file
+            SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*Zmumu*").scan(sh, inputFilePath); // Get all root files in this dataset
 
-			cross_section = 1901.2 * 1000.; // in fb
-			k_factor = 1.026;
-			filter_efficiency = 1.;
-			derivation_stat_weights = 34148906472.4;
-		}
-		else if (process == "ttbar") {
-			//SH::ScanDir().filePattern("ttbar_merged.root").scan(sh, inputFilePath); // Get specific root file
-			SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*ttbar*nonallhad*").scan(sh, inputFilePath); // Get all root files in this dataset
+            cross_section = 1901.2 * 1000.; // in fb
+            k_factor = 1.026;
+            filter_efficiency = 1.;
+            derivation_stat_weights = 34148906472.4;
+        }
+        else if (process == "ttbar") {
+            //SH::ScanDir().filePattern("ttbar_merged.root").scan(sh, inputFilePath); // Get specific root file
+            SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*ttbar*nonallhad*").scan(sh, inputFilePath); // Get all root files in this dataset
 
-			cross_section = 696.11 * 1000.; // in fb
-			k_factor = 1.1949;
-			filter_efficiency = 0.543;
-			derivation_stat_weights = 49386600.0;
-		}
-		else if (process == "GG_ttn1") {
-			//SH::ScanDir().filePattern("GG_ttn1_merged.root").scan(sh, inputFilePath); // Get specific root file
-			//SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*GG_ttn1_*.root").scan(sh, inputFilePath); // Get all root files in this dataset
+            cross_section = 696.11 * 1000.; // in fb
+            k_factor = 1.1949;
+            filter_efficiency = 0.543;
+            derivation_stat_weights = 49386600.0;
+        }
+        else if (process == "GG_ttn1") {
+            //SH::ScanDir().filePattern("GG_ttn1_merged.root").scan(sh, inputFilePath); // Get specific root file
+            //SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*GG_ttn1_*.root").scan(sh, inputFilePath); // Get all root files in this dataset
 
-			// First do an inclusive scan
-			SH::SampleHandler sh_gtt;
-			SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*GG_ttn1_*.root").scan(sh_gtt, inputFilePath); // Get all root files in this dataset	
+            // First do an inclusive scan
+            SH::SampleHandler sh_gtt;
+            SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*GG_ttn1_*.root").scan(sh_gtt, inputFilePath); // Get all root files in this dataset    
 
-			for (auto & sample : sh_gtt) {
-				// Get the full path of root file: /path/data_set/root_file
-				// We only need to know the data_set name, so the index file of fileName uses 0 is enough.
-				string filename = sample->fileName(0);
-				//cout << filename << endl;
-				int pos_GG_ttn1 = filename.find("GG_ttn1");
-				string str("GG_ttn1_");
-				int length_GG_ttn1_ = str.length();
-				int begin_pos = pos_GG_ttn1 + length_GG_ttn1_;
-				int pos_DAOD = filename.find(".DAOD"); // includes the "."
-				int length_to_retrieve = pos_DAOD - begin_pos;
-				string mass_values = filename.substr(begin_pos, length_to_retrieve);
-				int length_mass_values = mass_values.length();
-				int pos_delimiter1 = mass_values.find("_");
-				float m1 = stof(mass_values.substr(0, pos_delimiter1));
-				int pos_delimiter2 = mass_values.find("_", pos_delimiter1 + 1);
-				//float m2 = stof(mass_values.substr(pos_delimiter1 + 1, pos_delimiter2 - (pos_delimiter1 + 1)));
-				float m3 = stof(mass_values.substr(pos_delimiter2 + 1, length_mass_values - (pos_delimiter2 + 1)));
-				float delta_mass = m1 - m3;
-				//cout << "delta_mass=" << delta_mass << endl;
-				// Then filter the samples
-				if (isBoosted && // Boosted region
-					delta_mass > boosted_mass) {
-					sh.add(sample);
-				}
-				else if (!isBoosted) { // Compressed region
-					cout << "What is the definition of compressed region?" << endl;
-					// To be implemented.
-				}
-			}
-			// Couldn't find information. Set to 1.
-			cross_section = 1.;
-			k_factor = 1.;
-			filter_efficiency = 1.;
-			derivation_stat_weights = 4521452.27602;
-		}
-	}
-	else if (isData) {
-		cout << "Read Data files..." << endl;
-		inputFilePath = "/raid05/users/shen/Ximo_ntuples/v44/Data"; // no slash (/) at the end.
-		//SH::ScanDir().scan(sh, inputFilePath); // Get all datasets in inputFilePath
-		//SH::ScanDir().filePattern("merged_all_data.root").scan(sh, inputFilePath); // Get specific root file
-		SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*.physics_Main.DAOD_SUSY2.*").scan(sh, inputFilePath); // Get all root files in this dataset
-		// Set to 1 for data.
-		cross_section = 1.;
-		k_factor = 1.;
-		filter_efficiency = 1.;
-		derivation_stat_weights = 3587333027.0;
-	}
+            for (auto & sample : sh_gtt) {
+                // Get the full path of root file: /path/data_set/root_file
+                // We only need to know the data_set name, so the index file of fileName uses 0 is enough.
+                string filename = sample->fileName(0);
+                //cout << filename << endl;
+                int pos_GG_ttn1 = filename.find("GG_ttn1");
+                string str("GG_ttn1_");
+                int length_GG_ttn1_ = str.length();
+                int begin_pos = pos_GG_ttn1 + length_GG_ttn1_;
+                int pos_DAOD = filename.find(".DAOD"); // includes the "."
+                int length_to_retrieve = pos_DAOD - begin_pos;
+                string mass_values = filename.substr(begin_pos, length_to_retrieve);
+                int length_mass_values = mass_values.length();
+                int pos_delimiter1 = mass_values.find("_");
+                float m1 = stof(mass_values.substr(0, pos_delimiter1));
+                int pos_delimiter2 = mass_values.find("_", pos_delimiter1 + 1);
+                //float m2 = stof(mass_values.substr(pos_delimiter1 + 1, pos_delimiter2 - (pos_delimiter1 + 1)));
+                float m3 = stof(mass_values.substr(pos_delimiter2 + 1, length_mass_values - (pos_delimiter2 + 1)));
+                float delta_mass = m1 - m3;
+                //cout << "delta_mass=" << delta_mass << endl;
+                // Then filter the samples
+                if (isBoosted && // Boosted region
+                    delta_mass > boosted_mass) {
+                    sh.add(sample);
+                }
+                else if (!isBoosted) { // Compressed region
+                    cout << "What is the definition of compressed region?" << endl;
+                    // To be implemented.
+                }
+            }
+            // Couldn't find information. Set to 1.
+            cross_section = 1.;
+            k_factor = 1.;
+            filter_efficiency = 1.;
+            derivation_stat_weights = 4521452.27602;
+        }
+    }
+    else if (isData) {
+        cout << "Read Data files..." << endl;
+        inputFilePath = "/raid05/users/shen/Ximo_ntuples/v44/Data"; // no slash (/) at the end.
+        //SH::ScanDir().scan(sh, inputFilePath); // Get all datasets in inputFilePath
+        //SH::ScanDir().filePattern("merged_all_data.root").scan(sh, inputFilePath); // Get specific root file
+        SH::ScanDir().samplePattern("user.jpoveda.t0789_v44.*.physics_Main.DAOD_SUSY2.*").scan(sh, inputFilePath); // Get all root files in this dataset
+        // Set to 1 for data.
+        cross_section = 1.;
+        k_factor = 1.;
+        filter_efficiency = 1.;
+        derivation_stat_weights = 3587333027.0;
+    }
 
-	// Set the name of the input TTree.
-	sh.setMetaString("nc_tree", "AnaNtup");
+    // Set the name of the input TTree.
+    sh.setMetaString("nc_tree", "AnaNtup");
 
-	// Print what we found:
-	sh.print();
+    // Print what we found:
+    sh.print();
 /*
-	for (unsigned int sample_itr = 0; sample_itr < sh.size(); sample_itr++) {
-		SH::Sample *sample = sh.at(sample_itr);
-		cout << sample << endl;
-		cout << sample->name() << endl;
-		cout << sample->numFiles() << endl;
-		cout << sample->fileName(0) << endl;
-		// retrieve the histogram
-		//TH1D *hist = (TH1D *)sample->readHist("DerivationStat_Weights");
-		TH1D *hist = (TH1D *)sample->readHist("DerivationStat_Weights");
-		cout << hist << endl;
-		//cout << hist->GetBinContent(1) << endl;
-	}
+    for (unsigned int sample_itr = 0; sample_itr < sh.size(); sample_itr++) {
+        SH::Sample *sample = sh.at(sample_itr);
+        cout << sample << endl;
+        cout << sample->name() << endl;
+        cout << sample->numFiles() << endl;
+        cout << sample->fileName(0) << endl;
+        // retrieve the histogram
+        //TH1D *hist = (TH1D *)sample->readHist("DerivationStat_Weights");
+        TH1D *hist = (TH1D *)sample->readHist("DerivationStat_Weights");
+        cout << hist << endl;
+        //cout << hist->GetBinContent(1) << endl;
+    }
 */
 
-	// Create an EventLoop job:
-	EL::Job job;
-	job.sampleHandler( sh );
-	//job.options()->setDouble (EL::Job::optMaxEvents, 50);
+    // Create an EventLoop job:
+    EL::Job job;
+    job.sampleHandler( sh );
+    //job.options()->setDouble (EL::Job::optMaxEvents, 50);
 
-	// Add our analysis to the job:
-	ytEventSelection *alg = new ytEventSelection();
-	alg->set_isMC(isMC);
-	alg->set_isData(isData);
-	alg->set_isSkim(true);
-	if (isMC && !process.empty())
-		alg->set_process(process);
-	if (isData)
-		alg->set_process("Data");
-	alg->set_luminosity(luminosity);
-	alg->set_cross_section(cross_section);
-	alg->set_k_factor(k_factor);
-	alg->set_filter_efficiency(filter_efficiency);
-	alg->set_derivation_stat_weights(derivation_stat_weights);
-	job.algsAdd( alg );
+    // Add our analysis to the job:
+    ytEventSelection *alg = new ytEventSelection();
+    alg->set_isMC(isMC);
+    alg->set_isData(isData);
+    alg->set_isSkim(true);
+    if (isMC && !process.empty())
+        alg->set_process(process);
+    if (isData)
+        alg->set_process("Data");
+    alg->set_luminosity(luminosity);
+    alg->set_cross_section(cross_section);
+    alg->set_k_factor(k_factor);
+    alg->set_filter_efficiency(filter_efficiency);
+    alg->set_derivation_stat_weights(derivation_stat_weights);
+    job.algsAdd( alg );
 
-	if (use_Condor) {
-		// Run the jobs using the Condor driver:
-		EL::CondorDriver driver;
-		// some commands for setting up root on the nodes
-		driver.shellInit = "export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase ; source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh ; rcSetup Base,2.4.19";
-		driver.submitOnly( job, submitDir );
-	}
-	else if (use_Grid) {
-		// Run the jobs using the Grid driver:
-		EL::PrunDriver driver;
-		// Specify how to name the grid output datasets
-		// Note that %nickname% is populated when you do voms-proxy init, so this does not have to be replaced by hand
-		driver.options()->setString("nc_outputSampleName", "user.%nickname%.%in:name[2]%.%in:name[3]%.%in:name[6]%.");
-		driver.submitOnly( job, submitDir );
-	}
-	else if (use_PROOF) {
-		EL::ProofDriver driver;
-		driver.submit( job, submitDir );
-	}
-	else {
-		// Run the job using the local/direct driver:
-		EL::DirectDriver driver;
-		driver.submit( job, submitDir );
-	}
+    if (use_Condor) {
+        // Run the jobs using the Condor driver:
+        EL::CondorDriver driver;
+        // some commands for setting up root on the nodes
+        driver.shellInit = "export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase ; source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh ; rcSetup Base,2.4.19";
+        driver.submitOnly( job, submitDir );
+    }
+    else if (use_Grid) {
+        // Run the jobs using the Grid driver:
+        EL::PrunDriver driver;
+        // Specify how to name the grid output datasets
+        // Note that %nickname% is populated when you do voms-proxy init, so this does not have to be replaced by hand
+        driver.options()->setString("nc_outputSampleName", "user.%nickname%.%in:name[2]%.%in:name[3]%.%in:name[6]%.");
+        driver.submitOnly( job, submitDir );
+    }
+    else if (use_PROOF) {
+        EL::ProofDriver driver;
+        driver.submit( job, submitDir );
+    }
+    else {
+        // Run the job using the local/direct driver:
+        EL::DirectDriver driver;
+        driver.submit( job, submitDir );
+    }
 
-	return 0;
+    return 0;
 }
